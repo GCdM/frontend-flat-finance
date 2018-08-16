fcf0f474c7c916fd0fff1c8001e78e93522b9f88
GCdM <gabrielcm.br@gmail.com>
2018-08-16 15:09:19 +0100

    Basic architecture

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from 'react'
 
-class App extends Component {
+import './App.css'
+import NavBarContainer from './containers/NavBarContainer'
+import MainContentContainer from './containers/MainContentContainer'
+
+class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <NavBarContainer/>
+        <MainContentContainer/>
       </div>
-    );
+    )
   }
 }
 
